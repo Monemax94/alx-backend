87832cf1f97e38b9e7cc36b199330bfd47f1a270
Monemax94 <mondayodoh94gmail.com>
2023-04-25 23:55:53 +0100

    Task2 updated task3 done

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
-"""Bable setup with app module
+"""A Basic Flask app with internationalization support.
 """
-from flask import Flask, render_template, request
 from flask_babel import Babel
+from flask import Flask, render_template, request
 
 
 class Config:
-    """This class represents a Flask Babel
-    configuration.
+    """Represents a Flask Babel configuration.
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask('__name__')
+app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
@@ -22,7 +21,7 @@ babel = Babel(app)
 
 #@babel.localeselector
 def get_locale() -> str:
-    """Retrieves the locale for a web page
+    """Retrieves the locale for a web page.
     """
     babel.init_app(app, locale_selector=get_locale)
     return request.accept_languages.best_match(app.config["LANGUAGES"])
@@ -30,10 +29,9 @@ def get_locale() -> str:
 
 @app.route('/')
 def get_index() -> str:
-    """This function method
-    returns an index page.
+    """The home/index page.
     """
-    return render_template('1-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
